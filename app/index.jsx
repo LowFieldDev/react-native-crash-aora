c22146024089ecf86cc1aae3079e8330e37b4c13
Francesco Campobasso <francesco.campobasso@mia-platform.eu>
2025-01-29 09:17:10 +0100

    setup tabs

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar />
-      <Link href="/profile" style={{color: 'green'}}>Go to Profile</Link>
+      <Link href="/home" style={{color: 'green'}}>Go to Home</Link>
     </View>
   );
 }
